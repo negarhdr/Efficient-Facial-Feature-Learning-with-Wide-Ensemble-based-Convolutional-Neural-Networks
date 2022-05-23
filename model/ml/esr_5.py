e956b25b356646c2dd9c9ef500e738e40d88950a
Negar <negar.heidari@eng.au.dk>
2022-05-23 23:30:50 +0200

    discrete updated

---
+++ model/ml/esr_5.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Implementation of ESR-9 (Siqueira et al., 2020) trained on AffectNet (Mollahosseini et al., 2017) for emotion
+and affect perception.
+
+
+Reference:
+    Siqueira, H., Magg, S. and Wermter, S., 2020. Efficient Facial Feature Learning with Wide Ensemble-based
+    Convolutional Neural Networks. Proceedings of the Thirty-Fourth AAAI Conference on Artificial Intelligence
+    (AAAI-20), pages 1–1, New York, USA.
+
+    Mollahosseini, A., Hasani, B. and Mahoor, M.H., 2017. AffectNet: A database for facial expression, valence,
+    and arousal computing in the wild. IEEE Transactions on Affective Computing, 10(1), pp.18-31.
+"""
+
+__author__ = "Henrique Siqueira"
+__email__ = "siqueira.hc@outlook.com"
+__license__ = "MIT license"
+__version__ = "1.0"
+
+# Standard libraries
+from os import path
+
+# External libraries
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import torch.nn.init as init
+from os import path, makedirs
+import copy
+
+
+class CrossAttentionHead(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sa = SpatialAttention()
+        self.ca = ChannelAttention()
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        sa = self.sa(x)
+        ca = self.ca(sa)
+
+        return ca
+
+
+class SpatialAttention(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1x1 = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=1),
+            nn.BatchNorm2d(256),
+        )
+        self.conv_3x3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+        )
+        self.conv_1x3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=(1, 3), padding=(0, 1)),
+            nn.BatchNorm2d(512),
+        )
+        self.conv_3x1 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=(3, 1), padding=(1, 0)),
+            nn.BatchNorm2d(512),
+        )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        y = self.conv1x1(x)
+        y = self.relu(self.conv_3x3(y) + self.conv_1x3(y) + self.conv_3x1(y))
+        y = y.sum(dim=1, keepdim=True)
+        out = x * y
+
+        return out
+
+
+class ChannelAttention(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.attention = nn.Sequential(
+            nn.Linear(512, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 512),
+            nn.Sigmoid()
+        )
+
+    def forward(self, sa):
+        sa = self.gap(sa)
+        sa = sa.view(sa.size(0), -1)
+        y = self.attention(sa)
+        out = sa * y
+
+        return out
+
+
+class Base(nn.Module):
+    """
+        The base of the network (Ensembles with Shared Representations, ESRs) is responsible for learning low- and
+        mid-level representations from the input data that are shared with an ensemble of convolutional branches
+        on top of the architecture.
+
+        In our paper (Siqueira et al., 2020), it is called shared layers or shared representations.
+    """
+
+    def __init__(self):
+        super(Base, self).__init__()
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(3, 64, 5, 1)
+        self.conv2 = nn.Conv2d(64, 128, 3, 1)
+        self.conv3 = nn.Conv2d(128, 128, 3, 1)
+        self.conv4 = nn.Conv2d(128, 128, 3, 1)
+
+        # Batch-normalization layers
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        # Max-pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        # Convolutional, batch-normalization and pooling layers for representation learning
+        x_shared_representations = F.relu(self.bn1(self.conv1(x)))
+        x_shared_representations = self.pool(F.relu(self.bn2(self.conv2(x_shared_representations))))
+        x_shared_representations = F.relu(self.bn3(self.conv3(x_shared_representations)))
+        x_shared_representations = self.pool(F.relu(self.bn4(self.conv4(x_shared_representations))))
+
+        return x_shared_representations
+
+
+class ConvolutionalBranch(nn.Module):
+    """
+        Convolutional branches that compose the ensemble in ESRs. Each branch was trained on a sub-training
+        set from the AffectNet dataset to learn complementary representations from the data (Siqueira et al., 2020).
+
+        Note that, the second last layer provides eight discrete emotion labels whereas the last layer provides
+        continuous values of arousal and valence levels.
+    """
+
+    def __init__(self):
+        super(ConvolutionalBranch, self).__init__()
+
+        # Convolutional layers
+        self.conv1 = nn.Conv2d(128, 128, 3, 1)
+        self.conv2 = nn.Conv2d(128, 256, 3, 1)
+        self.conv3 = nn.Conv2d(256, 256, 3, 1)
+        self.conv4 = nn.Conv2d(256, 512, 3, 1, 1)
+
+        # Batch-normalization layers
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(512)
+
+        self.cross_attn = CrossAttentionHead()
+
+        # Second last, fully-connected layer related to discrete emotion labels
+        self.fc = nn.Linear(512, 8)
+
+        # Last, fully-connected layer related to continuous affect levels (arousal and valence)
+        self.fc_dimensional = nn.Linear(8, 2)
+
+        # Pooling layers
+        # Max-pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Global average pooling layer
+        self.global_pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x_shared_representations):
+        # Convolutional, batch-normalization and pooling layers
+        x_conv_branch = F.relu(self.bn1(self.conv1(x_shared_representations)))
+        x_conv_branch = self.pool(F.relu(self.bn2(self.conv2(x_conv_branch))))
+        x_conv_branch = F.relu(self.bn3(self.conv3(x_conv_branch)))
+        x_conv_branch = F.relu(self.bn4(self.conv4(x_conv_branch)))
+
+        attn_head = self.cross_attn(x_conv_branch)  # attention head output
+        print('attn_head size', attn_head.shape)
+
+        # I think we can comment the next two lines (gap, reshape) and pass the attn_head to the fc & fc_dimensional
+        x_conv_branch = self.global_pool(x_conv_branch)
+        x_conv_branch = x_conv_branch.view(-1, 512)
+
+        # Fully connected layer for emotion perception
+        discrete_emotion = self.fc(x_conv_branch)
+
+        # Application of the ReLU function to neurons related to discrete emotion labels
+        x_conv_branch = F.relu(discrete_emotion)
+
+        # Fully connected layer for affect perception
+        continuous_affect = self.fc_dimensional(x_conv_branch)
+
+        # Returns activations of the discrete emotion output layer and arousal and valence levels
+        return discrete_emotion, continuous_affect, attn_head
+
+
+class ESR(nn.Module):
+    """
+    ESR is the unified ensemble architecture composed of two building blocks the Base and ConvolutionalBranch
+    classes as described below by Siqueira et al. (2020):
+
+    'An ESR consists of two building blocks. (1) The base (class Base) of the network is an array of convolutional
+    layers for low- and middle-level feature learning. (2) These informative features are then shared with
+    independent convolutional branches (class ConvolutionalBranch) that constitute the ensemble.'
+    """
+
+    # Default values
+    # Input size
+    INPUT_IMAGE_SIZE = (96, 96)
+    # Values for pre-processing input data
+    INPUT_IMAGE_NORMALIZATION_MEAN = [0.0, 0.0, 0.0]
+    INPUT_IMAGE_NORMALIZATION_STD = [1.0, 1.0, 1.0]
+    # Path to saved network
+    PATH_TO_SAVED_NETWORK = "./model/ml/trained_models/esr_9"
+    FILE_NAME_BASE_NETWORK = "Net-Base-Shared_Representations.pt"
+    FILE_NAME_CONV_BRANCH = "Net-Branch_{}.pt"
+
+    def __init__(self, device):
+        """
+        Loads ESR-9.
+
+        :param device: Device to load ESR-9: GPU or CPU.
+        """
+
+        super(ESR, self).__init__()
+
+        # Base of ESR-9 as described in the docstring (see mark 1)
+        self.base = Base()
+        self.device = device
+        self.base.to(self.device)
+
+        # Load 9 convolutional branches that composes ESR-9 as described in the docstring (see mark 2)
+        self.convolutional_branches = []
+
+        self.to(device)
+
+        self.attn_fc = nn.Linear(512, 8)
+        self.attn_bn = nn.BatchNorm1d(8)
+        # Evaluation mode on
+        # self.eval()
+
+    def get_ensemble_size(self):
+        return len(self.convolutional_branches)
+
+    def add_branch(self):
+        self.convolutional_branches.append(ConvolutionalBranch())
+        self.convolutional_branches[-1].to(self.device)
+
+    @staticmethod
+    def save(state_dicts, base_path_to_save_model, current_branch_save):
+        if not path.isdir(path.join(base_path_to_save_model, str(current_branch_save))):
+            makedirs(path.join(base_path_to_save_model, str(current_branch_save)))
+
+        torch.save(state_dicts[0],
+                   path.join(base_path_to_save_model,
+                             str(current_branch_save),
+                             "Net-Base-Shared_Representations.pt"))
+
+        for i in range(1, len(state_dicts)):
+            torch.save(state_dicts[i],
+                       path.join(base_path_to_save_model,
+                                 str(current_branch_save),
+                                 "Net-Branch_{}.pt".format(i)))
+
+        print("Network has been "
+              "saved at: {}".format(path.join(base_path_to_save_model, str(current_branch_save))))
+
+    def to_state_dict(self):
+        state_dicts = [copy.deepcopy(self.base.state_dict())]
+
+        for b in self.convolutional_branches:
+            state_dicts.append(copy.deepcopy(b.state_dict()))
+
+        return state_dicts
+
+    def to_device(self, device_to_process="cpu"):
+        self.to(device_to_process)
+        self.base.to(device_to_process)
+
+        for b_td in self.convolutional_branches:
+            b_td.to(device_to_process)
+
+    def reload(self, best_configuration):
+        self.base.load_state_dict(best_configuration[0])
+
+        for i in range(self.get_ensemble_size()):
+            self.convolutional_branches[i].load_state_dict(best_configuration[i + 1])
+
+    def forward(self, x):
+        """
+        Forward method of ESR-9.
+
+        :param x: (ndarray) Input data.
+        :return: A list of emotions and affect values from each convolutional branch in the ensemble.
+        """
+
+        # List of emotions and affect values from the ensemble
+        emotions = []
+        affect_values = []
+        attn_heads = []
+
+        # Get shared representations
+        x_shared_representations = self.base(x)
+
+        # Add to the lists of predictions outputs from each convolutional branch in the ensemble
+        for branch in self.convolutional_branches:
+            output_emotion, output_affect, attention_head = branch(x_shared_representations)
+            emotions.append(output_emotion)
+            affect_values.append(output_affect)
+            attn_heads.append(attention_head)
+
+        heads = torch.stack(attn_heads).permute([1, 0, 2])
+        if heads.size(1) > 1:
+            heads = F.log_softmax(heads, dim=1)
+
+        # attn_out = self.attn_fc(heads.sum(dim=1))  # or we can remove the sum and in branch, apply log_softmax and then fc to produce sth with the same size of emotion/dimension outputs
+        # attn_out = self.attn_bn(attn_out)
+
+        return emotions, affect_values, heads #, attn_out
+
