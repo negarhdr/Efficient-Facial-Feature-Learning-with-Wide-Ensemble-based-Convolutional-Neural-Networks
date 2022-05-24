8ba6eafb18097f9b77d8fcc4ef06e44f164acfba
Negar <negar.heidari@eng.au.dk>
2022-05-24 13:29:13 +0200

    discrete updated

--- attn_affectnet_discrete.py
+++ attn_affectnet_discrete.py
@@ -142,7 +142,7 @@ class FeatureDiversity(nn.Module):
         diff = 0
         for i in range(num_features):
             for j in range(num_features):
-                diff += torch.square(x[:, i] - x[:, j])
+                diff += (x[:, i] - x[:, j]) ^ 2
         print('diff', diff)
         diff = 1/(2*num_features*(num_features-1)) * diff
         div = diff.mean()
