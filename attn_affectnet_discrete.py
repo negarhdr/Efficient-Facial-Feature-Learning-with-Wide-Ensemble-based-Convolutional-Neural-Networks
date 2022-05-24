db6d50bce33cff4c5af3fe8d5ff9701e37a5664a
Negar <negar.heidari@eng.au.dk>
2022-05-24 14:47:21 +0200

    discrete updated

--- attn_affectnet_discrete.py
+++ attn_affectnet_discrete.py
@@ -255,6 +255,7 @@ def main():
                     loss += criterion(emotions[i_4], labels)
 
                 loss += attn_criterion(heads)    # partition loss between different attention heads (maximize the difference between them)
+                print('atten_loss', attn_criterion(heads) )
                 # div = diversity(heads)  # diversity between different channels of attention
                 # print('loss', div)
 
