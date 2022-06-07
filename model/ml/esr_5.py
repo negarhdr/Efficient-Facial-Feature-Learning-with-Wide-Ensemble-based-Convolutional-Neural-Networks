baf0fba73b8e7857f35d6d81b4858fac33b693c2
Negar <negar.heidari@eng.au.dk>
2022-06-07 23:33:54 +0200

    discrete updated

--- model/ml/esr_5.py
+++ model/ml/esr_5.py
@@ -85,11 +85,9 @@ class SpatialAttention(nn.Module):
 
     def forward(self, x):
         y = self.conv1x1(x)
-        print('y,', y)
         y = self.relu(self.conv_3x3(y) + self.conv_1x3(y) + self.conv_3x1(y))
         y = y.sum(dim=1, keepdim=True)
         out = x * y  # torch.Size([32, 512, 6, 6])
-
 
         return out
 
@@ -202,6 +200,7 @@ class ConvolutionalBranch(nn.Module):
         x_conv_branch = self.pool(F.relu(self.bn2(self.conv2(x_conv_branch))))
         x_conv_branch = F.relu(self.bn3(self.conv3(x_conv_branch)))
         x_conv_branch = F.relu(self.bn4(self.conv4(x_conv_branch)))
+        print('x_branch', x_conv_branch)
 
         attn_head = self.cross_attn(x_conv_branch)  # attention head output # N x 512
         # print('attn_head', attn_head)
