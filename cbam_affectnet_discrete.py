382b3055a6bd6f9ee4201355b510572c92485644
Negar <negar.heidari@eng.au.dk>
2022-07-18 23:17:53 +0200

    discrete updated

--- cbam_affectnet_discrete.py
+++ cbam_affectnet_discrete.py
@@ -119,7 +119,7 @@ def plot(his_loss, his_acc, his_val_loss, his_val_acc, branch_idx, base_path_his
 def main():
     # Experimental variables
     base_path_experiment = "./experiments/AffectNet_Discrete/"
-    name_experiment = "CBAM_ESR_1_AffectNet_Discrete"
+    name_experiment = "CBAM_ESR_9_AffectNet_Discrete"
     base_path_to_dataset = "../FER_data/AffectNet/"
     num_branches_trained_network = 9
     validation_interval = 1
