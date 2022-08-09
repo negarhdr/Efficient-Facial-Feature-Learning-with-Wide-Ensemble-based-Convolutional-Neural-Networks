6e3658be975cc07f7553abead48fb8d018a3bbda
Negar <negar.heidari@eng.au.dk>
2022-08-10 00:31:01 +0200

    residual added

--- main_esr_9_bregnext_residual.py
+++ main_esr_9_bregnext_residual.py
@@ -16,7 +16,7 @@ __license__ = "MIT license"
 __version__ = "1.0"
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 # External Libraries
 from torch.utils.data import DataLoader
