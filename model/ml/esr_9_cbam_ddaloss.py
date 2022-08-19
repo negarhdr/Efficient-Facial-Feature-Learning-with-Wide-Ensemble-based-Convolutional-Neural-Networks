46077318628b9609b88108688068b9e0bc6407ce
Negar <negar.heidari@eng.au.dk>
2022-08-19 23:49:26 +0200

    residual added

--- model/ml/esr_9_cbam_ddaloss.py
+++ model/ml/esr_9_cbam_ddaloss.py
@@ -226,8 +226,8 @@ class ESR(nn.Module):
         self.base.to(self.device)
 
         # Class centers
-        self.centers = nn.Parameter(torch.FloatTensor(8, 512))
-        nn.init.kaiming_normal_(self.centers.data.t())  # might not be needed
+        self.centers = nn.Parameter(torch.FloatTensor(512, 8))
+        # nn.init.kaiming_normal_(self.centers.data.t())  # might not be needed
 
         # Load 9 convolutional branches that composes ESR-9 as described in the docstring (see mark 2)
         self.convolutional_branches = []
