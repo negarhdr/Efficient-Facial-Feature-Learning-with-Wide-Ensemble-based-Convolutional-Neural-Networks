665958f2bfc8f2022703a86a79b76ceee8b13548
Negar <negar.heidari@eng.au.dk>
2022-08-23 16:41:48 +0200

    residual added

--- cbam_ddaloss_affectnet_discrete.py
+++ cbam_ddaloss_affectnet_discrete.py
@@ -216,7 +216,8 @@ def main(args):
 
     # Set optimizer
     optimizer = optim.SGD([{'params': net.base.parameters(), 'lr': 0.1, 'momentum': 0.9},
-                           {'params': net.convolutional_branches[-1].parameters(), 'lr': 0.1, 'momentum': 0.9}])
+                           {'params': net.convolutional_branches[-1].parameters(), 'lr': 0.1, 'momentum': 0.9},
+                           {'params': DDA_Loss.parameters(), 'lr': 0.1, 'momentum': 0.9}])
 
 
     # Define criterion
@@ -363,7 +364,8 @@ def main(args):
             # Set optimizer for base and the new branch
             optimizer = optim.SGD([{'params': net.base.parameters(), 'lr': 0.01, 'momentum': 0.9},
                                    {'params': net.convolutional_branches[-1].parameters(), 'lr': 0.1,
-                                    'momentum': 0.9}])
+                                    'momentum': 0.9},
+                                   {'params': DDA_Loss.parameters(), 'lr': 0.1, 'momentum': 0.9}])
 
             # Set optimizer for the trained branches
             if not args.freeze_trained_branches:
