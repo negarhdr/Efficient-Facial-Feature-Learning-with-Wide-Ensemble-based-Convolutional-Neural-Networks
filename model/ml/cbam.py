6e58eca065f4721e60d3115562149dcb6acea652
Negar <negar.heidari@eng.au.dk>
2022-08-24 18:42:05 +0200

    residual added

--- model/ml/cbam.py
+++ model/ml/cbam.py
@@ -102,6 +102,7 @@ class SpatialGate(nn.Module):
         feat_dim = x.shape[1]
         masked_ch_features = self.avg_pool2d(x).view(-1, feat_dim)
         masked_sp_features = (x * scale)
+        print(masked_sp_features.shape)
         attn_sp = torch.nn.Tanh(masked_sp_features)
         attn_ch = torch.nn.Tanh(masked_ch_features)
         # attn_mask = torch.sigmoid(x_out)  # this is the one I was using for diversity until now
