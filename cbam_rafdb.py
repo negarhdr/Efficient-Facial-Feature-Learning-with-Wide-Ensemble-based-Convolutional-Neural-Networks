8aa16644586e2905e21509013a84138e0c8635eb
Negar <negar.heidari@eng.au.dk>
2022-09-18 23:33:35 +0200

    residual added

--- cbam_rafdb.py
+++ cbam_rafdb.py
@@ -267,7 +267,7 @@ def plot(his_loss, his_acc, his_val_loss, his_val_acc, branch_idx, base_path_his
 def main():
     # Experimental variables
     base_path_experiment = "./experiments/RAFDB/"
-    name_experiment = "ESR_9_div_dda_ESR_9_rafdb_1"
+    name_experiment = "ESR_9_div_dda_ESR_9_rafdb_2"
     base_path_to_dataset = "../FER_data/RAF-DB/basic/"
     num_branches_trained_network = 9
     validation_interval = 2
